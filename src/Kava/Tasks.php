656ab470ce0e691a4e598076aaf4394361c63848
Adrian Pietka <adrian@pietka.me>
2015-09-25 23:47:28 +0200

    Task name must be a unique

--- src/Kava/Tasks.php
+++ src/Kava/Tasks.php
@@ -6,11 +6,25 @@ class Tasks {
     private $tasks = [];
     
     public function add(Task $task) {
+        if (!$this->hasUniqueTaskName($task->name())) {
+            throw new Exception(sprintf('Task name (%s) is not unique!', $task->name()));
+        }
+
         $this->tasks[] = $task;
     }
     
     public function all() {
         return $this->tasks;
+    }
+
+    public function hasUniqueTaskName($taskName) {
+        foreach($this->tasks as $task) {
+            if ($task->name() === $taskName) {
+                return false;
+            }
+        }
+
+        return true;
     }
     
     public function get($taskName) {
